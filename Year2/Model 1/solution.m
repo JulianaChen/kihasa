b0bbea6948f02e0eacd51a3fb29f349303237dca
Juliana Chen <jctwinkle@gmail.com>
2018-06-13 12:22:48 -0700

    added validation and saving optimal choice for marriage with 2 kids. still need to figure out which labor choice is taken and to reshape the policy functions for the simulation

--- Year2/Model 1/solution.m
+++ Year2/Model 1/solution.m
@@ -1,6 +1,6 @@
 function [c_func,m_func,lr_func,ln_func,lu_func] = solution(G,abi,edu,S,params)
 
-% Index for parameters
+%% index for parameters
 % params0 = [psi_r;psi_n;theta;alpha;sigma_r;sigma_n;sigma_i;omega;lambda;eta;iota;kappa;tau];
     
     psi_r=params(1); % disutility of work by sector (regular)
@@ -77,6 +77,7 @@ function [c_func,m_func,lr_func,ln_func,lu_func] = solution(G,abi,edu,S,params)
     tau22 = params(63); % probability of losing a regular job (4yr college)
     tau23 = params(64); % probability of losing a regular job (age)
     tau24 = params(65); % probability of losing a regular job (work exp)
+    
     % move to params
     phi10 = 3.679;
     phi11 = -2.89;
@@ -90,7 +91,7 @@ function [c_func,m_func,lr_func,ln_func,lu_func] = solution(G,abi,edu,S,params)
     phi31 = -0.898;
     phi32 = -1.69;
     phi33 = -0.379;
-    
+
 %% Terminal Value Function:
 
 % age at TVF
@@ -98,12 +99,12 @@ age_TVF = 18*(edu==1) + 20*(edu==2) + 22*(edu==3) + G.n_period;
 
 % assets
 assets = S.SS_A; 
-    
+
 % husband wages
 wh_mean = eta01 + eta02*(abi==2) + eta11*(edu==2) + eta12*(edu==3) + eta2*age_TVF;
 wh_sd = eta03 + eta04*(abi==2) + eta21*(edu==2) + eta22*(edu==3) + eta3*age_TVF;
 wh = normrnd(wh_mean,wh_sd);
-    
+
 % child human capital 
 Inv_mean = iota01 + iota02*(abi==2) + iota11*(edu==2) + iota12*(edu==3) + iota2*age_TVF;
 Inv_sd = iota03 + iota04*(abi==2) + iota21*(edu==2) + iota22*(edu==3) + iota3*age_TVF;
@@ -140,7 +141,7 @@ for t = G.n_period-1:-1:1
     
     % child human capital 
     K = kappa01 + kappa02*(abi==2) + kappa03*(edu==2) + kappa04*(edu==3) + kappa05*(Inv); %function of women's ability, education
-  
+    
     % marriage probabilities 
     prob_marr_w = omega0_w + omega11*(edu==2) + omega12*(edu==3) + omega2*age;
     prob_marr_u = omega0_u + omega11*(edu==2) + omega12*(edu==3) + omega2*age;
@@ -148,17 +149,17 @@ for t = G.n_period-1:-1:1
     % loop for work experience and marital status (20): %%%%% change to 30
     for x = 1:1:(G.n_matstat*G.n_wrkexp)
         x;
-    
+        
         % current state discrete variables:
         m_j = S.SS_M(x);  % marital status
         n_j = S.SS_N(x);  % children
         X_j = S.SS_X(x);  % experience
-    
+        
         % job probabilities %% how to make sure the prob is in [0 1] - use
         % probit 
         prob_lamba = tau10 + tau11*(edu==2) + tau12*(edu==3) + tau13*age + tau14*X_j; % probability of losing a regular job
         prob_pi = tau20 + tau21*(edu==2) + tau22*(edu==3) + tau23*age + tau24*X_j; % probability of getting a regular job
-    
+        
         % child probabilities  %% how to make sure the prob is in [0 1]
         prob_2kids_r = phi10 + phi11*(edu==2) + phi12*(edu==3) + phi13*X_j;
         prob_2kids_n = phi20 + phi21*(edu==2) + phi22*(edu==3) + phi23*X_j;
@@ -167,7 +168,7 @@ for t = G.n_period-1:-1:1
         % loop for shocks (9):
         for i = 1:1:G.n_shocks
             i;
-        
+            
             shock_i = S.shocks_i(i);
             shock_r = S.shocks_r(i);
             shock_n = S.shocks_n(i);
@@ -180,10 +181,10 @@ for t = G.n_period-1:-1:1
             % loop over assets
             for j = 1:1:G.n_SS
                 j;
-            
+                
                 % HH's assets
                 A_j = S.SS_A(j); 
-            
+                
                 % consumption vector
                 chh_min = 0.1; %  move outside
                 chh_r = w_j_r + wh*m_j + A_j;
@@ -195,9 +196,10 @@ for t = G.n_period-1:-1:1
                 cr_vector = linspace(chh_min,chh_r_max,G.n_cons);
                 cn_vector = linspace(chh_min,chh_n_max,G.n_cons);
                 cu_vector = linspace(chh_min,chh_u_max,G.n_cons);
-
+                
                 % wide assets vector for linear interpolation % move -10 to params
-                A_wide = linspace(-10,max(S.SS_A),length(unique(S.SS_A))); % need?
+                A_min = -10;
+                A_wide = linspace(A_min,max(S.SS_A),length(unique(S.SS_A))); % need?
                 
                 % loop over consumption
                 for k = 1:1:G.n_cons
@@ -284,7 +286,7 @@ for t = G.n_period-1:-1:1
                         Vm_r(k) = u_r(k) + G.beta * ((prob_lamba*Vm_r_next_linear)+(1-prob_lamba)*Vm_r_next_linear2);
                         Vm_n(k) = u_n(k) + G.beta * ((prob_pi*Vm_n_next_linear)+(1-prob_pi)*Vm_n_next_linear2);
                         Vm_u(k) = u_u(k) + G.beta * ((prob_pi*Vm_u_next_linear)+(1-prob_pi)*Vm_u_next_linear2);
-
+                    
                     % Married with 2 kids: %% Julie will add this part
                     elseif x <= 20
                         
@@ -352,6 +354,7 @@ for t = G.n_period-1:-1:1
                         Vm2_r(k) = u_r(k) + G.beta * ((prob_lamba*Vm2_r_next_linear)+(1-prob_lamba)*Vm2_r_next_linear2);
                         Vm2_n(k) = u_n(k) + G.beta * ((prob_pi*Vm2_n_next_linear)+(1-prob_pi)*Vm2_n_next_linear2);
                         Vm2_u(k) = u_u(k) + G.beta * ((prob_pi*Vm2_u_next_linear)+(1-prob_pi)*Vm2_u_next_linear2);
+                        
                         % save marriage values (for marriage decision)
                         Vm_r_aux(k,x) = Vm2_r(k)*(prob_2kids_r) + Vm_r(k)*(1-prob_2kids_r);
                         Vm_n_aux(k,x) = Vm2_n(k)*(prob_2kids_n) + Vm_n(k)*(1-prob_2kids_n);
@@ -429,73 +432,101 @@ for t = G.n_period-1:-1:1
                         Vsm_u(k) = prob_marr_u*Vm_u_aux(k,x-10) + (1-prob_marr_u)*Vs_u(k);
                     end
                 end
-
-                % optimal consumption and max Emax
+                
+                % optimal consumption and max VF
                 if x <= 10
                     % check
-                    Vm_r(Amr_next < -10) = NaN;
+                    Vm_r(Amr_next < A_min) = NaN;
                     Vm_r(Amr_next > max(S.SS_A)) = NaN;
-                    Vm_n(Amn_next < -10) = NaN;
+                    Vm_n(Amn_next < A_min) = NaN;
                     Vm_n(Amn_next > max(S.SS_A)) = NaN;
-                    Vm_u(Amu_next < -10) = NaN;
+                    Vm_u(Amu_next < A_min) = NaN;
                     Vm_u(Amu_next > max(S.SS_A)) = NaN;
                     % save optimal
-                    [Vm_r_star, Index_mr_k] = max(Vm_r);
-                    [Vm_n_star, Index_mn_k] = max(Vm_n);
-                    [Vm_u_star, Index_mu_k] = max(Vm_u);
-                    cm_r_star = cr_vector(Index_mr_k);
-                    cm_n_star = cn_vector(Index_mn_k);
-                    cm_u_star = cu_vector(Index_mu_k);
+                    [Vm_r_star, index_mr_k] = max(Vm_r);
+                    [Vm_n_star, index_mn_k] = max(Vm_n);
+                    [Vm_u_star, index_mu_k] = max(Vm_u);
+                    cm_r_star = cr_vector(index_mr_k);
+                    cm_n_star = cn_vector(index_mn_k);
+                    cm_u_star = cu_vector(index_mu_k);
                     cm_star_aux = [cm_r_star, cm_n_star, cm_u_star];
-                    [Vm_star, Index_lm] = max([Vm_r_star,Vm_n_star,Vm_u_star]); % with 3 job options
-                    [Vm_star2, Index_lm2] = max([Vm_n_star,Vm_u_star]); % is this emax2 (2 job options?)
-                else %%%% add 2 kids?
+                    [Vm_star, lm_index] = max([Vm_r_star,Vm_n_star,Vm_u_star]); % 3 job options
+                    [Vm_star2, lm_index2] = max([Vm_n_star,Vm_u_star]); % emax2 (2 job options)
+                elseif x <= 20
                     % check
-                    Vs_r(Asr_next < -10) = NaN;
+                    Vm2_r(Am2r_next < A_min) = NaN;
+                    Vm2_r(Am2r_next > max(S.SS_A)) = NaN;
+                    Vm2_n(Am2n_next < A_min) = NaN;
+                    Vm2_n(Am2n_next > max(S.SS_A)) = NaN;
+                    Vm2_u(Am2u_next < A_min) = NaN;
+                    Vm2_u(Am2u_next > max(S.SS_A)) = NaN;
+                    % save optimal
+                    [Vm2_r_star, index_m2r_k] = max(Vm_r);
+                    [Vm2_n_star, index_m2n_k] = max(Vm_n);
+                    [Vm2_u_star, index_m2u_k] = max(Vm_u);
+                    cm2_r_star = cr_vector(index_m2r_k);
+                    cm2_n_star = cn_vector(index_m2n_k);
+                    cm2_u_star = cu_vector(index_m2u_k);
+                    cm2_star_aux = [cm2_r_star, cm2_n_star, cm2_u_star];
+                    [Vm2_star, lm2_index] = max([Vm2_r_star,Vm2_n_star,Vm2_u_star]); % 3 job options
+                    [Vm2_star2, lm2_index2] = max([Vm2_n_star,Vm2_u_star]); % emax2 (2 job options)
+                else
+                    % check
+                    Vs_r(Asr_next < A_min) = NaN;
                     Vs_r(Asr_next > max(S.SS_A)) = NaN;
-                    Vs_n(Asn_next < -10) = NaN;
+                    Vs_n(Asn_next < A_min) = NaN;
                     Vs_n(Asn_next > max(S.SS_A)) = NaN;
-                    Vs_u(Asu_next < -10) = NaN;
+                    Vs_u(Asu_next < A_min) = NaN;
                     Vs_u(Asu_next > max(S.SS_A)) = NaN;
                     % save optimal
-                    [Vs_r_star, Index_sr_k] = max(Vs_r);
-                    [Vs_n_star, Index_sn_k] = max(Vs_n);
-                    [Vs_u_star, Index_su_k] = max(Vs_u);
-                    [Vsm_r_star, Index_smr_k] = max(Vsm_r);
-                    [Vsm_n_star, Index_smn_k] = max(Vsm_n);
-                    [Vsm_u_star, Index_smu_k] = max(Vsm_u);
-                    cs_r_star = cr_vector(Index_sr_k);
-                    cs_n_star = cn_vector(Index_sn_k);
-                    cs_u_star = cu_vector(Index_su_k);
-                    csm_r_star = cr_vector(Index_smr_k);
-                    csm_n_star = cn_vector(Index_smn_k);
-                    csm_u_star = cu_vector(Index_smu_k);
+                    [Vs_r_star, index_sr_k] = max(Vs_r);
+                    [Vs_n_star, index_sn_k] = max(Vs_n);
+                    [Vs_u_star, index_su_k] = max(Vs_u);
+                    [Vsm_r_star, index_smr_k] = max(Vsm_r);
+                    [Vsm_n_star, index_smn_k] = max(Vsm_n);
+                    [Vsm_u_star, index_smu_k] = max(Vsm_u);
+                    cs_r_star = cr_vector(index_sr_k);
+                    cs_n_star = cn_vector(index_sn_k);
+                    cs_u_star = cu_vector(index_su_k);
+                    csm_r_star = cr_vector(index_smr_k);
+                    csm_n_star = cn_vector(index_smn_k);
+                    csm_u_star = cu_vector(index_smu_k);
                     cs_star_aux = [csm_r_star, csm_n_star, csm_u_star, cs_r_star, cs_n_star, cs_u_star];
-                    [Vs_star, Index_ls] = max([Vsm_r_star, Vsm_n_star, Vsm_u_star, Vs_r_star, Vs_n_star, Vs_u_star]);
-                    [Vs_star2, Index_ls2] = max([Vsm_n_star, Vsm_u_star, Vs_n_star, Vs_u_star]); % is this emax2?
+                    [Vs_star, ls_index] = max([Vsm_r_star, Vsm_n_star, Vsm_u_star, Vs_r_star, Vs_n_star, Vs_u_star]);
+                    [Vs_star2, ls_index2] = max([Vsm_n_star, Vsm_u_star, Vs_n_star, Vs_u_star]);
                 end
+            
             % save choice:
-            if x <= 10 %20
-                c_star(j, i, x, t) = cm_star_aux(Index_lm);
-                l_star(j, i, x, t) = Index_lm; % which L choice?
+            if x <= 10
+                c_star(j, i, x, t) = cm_star_aux(lm_index);
+                l_star(j, i, x, t) = lm_index; % which L choice?
                 V_star(j, i, x, t) = Vm_star;
                 V2_star(j,i, x, t) = Vm_star2;
-            else %%%% add 2 kids?
-                c_star(j, i, x, t) = cs_star_aux(Index_ls);
-                l_star(j, i, x, t) = Index_ls; % which L choice?
+            elseif x <= 20
+                c_star(j, i, x, t) = cm2_star_aux(lm2_index);
+                l_star(j, i, x, t) = lm2_index; % which L choice?
+                V_star(j, i, x, t) = Vm2_star;
+                V2_star(j,i, x, t) = Vm2_star2;
+            else
+                c_star(j, i, x, t) = cs_star_aux(ls_index);
+                l_star(j, i, x, t) = ls_index; % which L choice?
                 V_star(j, i, x, t) = Vs_star;
                 V2_star(j,i, x, t) = Vs_star2;
             end
             
             % save the number assets outside grid
             if x <= 10
-                Ar_out(j,i,x,t) = sum(Amr_next < -10) + sum(Amr_next > max(S.SS_A));
-                An_out(j,i,x,t) = sum(Amn_next < -10) + sum(Amn_next > max(S.SS_A));
-                Au_out(j,i,x,t) = sum(Amu_next < -10) + sum(Amu_next > max(S.SS_A));
-            else %%%% add 2 kids?
-                Ar_out(j,i,x,t) = sum(Asr_next < -10) + sum(Asr_next > max(S.SS_A));
-                An_out(j,i,x,t) = sum(Asn_next < -10) + sum(Asn_next > max(S.SS_A));
-                Au_out(j,i,x,t) = sum(Asu_next < -10) + sum(Asu_next > max(S.SS_A));
+                Ar_out(j,i,x,t) = sum(Amr_next < A_min) + sum(Amr_next > max(S.SS_A));
+                An_out(j,i,x,t) = sum(Amn_next < A_min) + sum(Amn_next > max(S.SS_A));
+                Au_out(j,i,x,t) = sum(Amu_next < A_min) + sum(Amu_next > max(S.SS_A));
+            elseif x <= 20
+                Ar_out(j,i,x,t) = sum(Am2r_next < A_min) + sum(Am2r_next > max(S.SS_A));
+                An_out(j,i,x,t) = sum(Am2n_next < A_min) + sum(Am2n_next > max(S.SS_A));
+                Au_out(j,i,x,t) = sum(Am2u_next < A_min) + sum(Am2u_next > max(S.SS_A));
+            else
+                Ar_out(j,i,x,t) = sum(Asr_next < A_min) + sum(Asr_next > max(S.SS_A));
+                An_out(j,i,x,t) = sum(Asn_next < A_min) + sum(Asn_next > max(S.SS_A));
+                Au_out(j,i,x,t) = sum(Asu_next < A_min) + sum(Asu_next > max(S.SS_A));
             end
             end
         end
