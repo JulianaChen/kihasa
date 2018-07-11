b52d1249bfb6bf831daa4da226070a4e67f5d861
Juliana Chen <jctwinkle@gmail.com>
2018-07-11 16:41:46 -0700

    Added Italo's files and changes for the mdf (estimation).

--- Year2/Model 1/Main.m
+++ Year2/Model 1/Main.m
@@ -7,64 +7,70 @@ clear all; clc;
 %% Set up Parameters
 run Setup_Parameters.m
 
-%% Temporary:
-z=1;
-abi=types(z,1);
-edu=types(z,2);
-params=params0;
-
 %% Set up State Space
 S = sspace(params0,G);
 
-%% Solution
-tic;
-parfor z=1:1:G.n_incond
-    z
-    [C(:,:,:,:,:,z),M(:,:,:,:,:,z),R(:,:,:,:,:,z),N(:,:,:,:,:,z),U(:,:,:,:,:,z)]= solution(G,types(z,1),types(z,2),S,params0);
-    toc
-end
+% %% Drawing Types
+% for n=1:1:G.n_pop
+%         seed(n)=rand;
+%         if seed(n)<0.16
+%             type(n,1)=1;
+%             abi(n,1)=1;
+%             edu(n,1)=1;
+%         elseif seed(n)<0.189 && seed(n)>=0.16
+%             type(n,1)=2;
+%             abi(n,1)=1;
+%             edu(n,1)=2;
+%         elseif seed(n)<0.215 && seed(n)>=0.189
+%             type(n,1)=3;
+%             abi(n,1)=1;
+%             edu(n,1)=3;
+%         elseif seed(n)<0.579 && seed(n)>=0.215
+%             type(n,1)=4;
+%             abi(n,1)=2;
+%             edu(n,1)=1;
+%         elseif seed(n)<0.73 && seed(n)>=0.579
+%             type(n,1)=5;
+%             abi(n,1)=2;
+%             edu(n,1)=2;
+%         elseif seed(n)<=1 && seed(n)>=0.73
+%             type(n,1)=6;
+%             abi(n,1)=2;
+%             edu(n,1)=3;
+%         end
+% end
 
-%save solution.mat
+% %% Solution
+% tic;
+% parfor z=1:1:G.n_incond
+%     z
+%     [C(:,:,:,:,:,z),M(:,:,:,:,:,z),R(:,:,:,:,:,z),N(:,:,:,:,:,z),U(:,:,:,:,:,z)]= solution(G,types(z,1),types(z,2),S,params0);
+%     toc
+% end
+% 
+% save solution.mat
 
-%% Drawing Types
-for n=1:1:G.n_pop
-        seed(n)=rand;
-        if seed(n)<0.16
-            type(n,1)=1;
-            abi(n,1)=1;
-            edu(n,1)=1;
-        elseif seed(n)<0.189 && seed(n)>=0.16
-            type(n,1)=2;
-            abi(n,1)=1;
-            edu(n,1)=2;
-        elseif seed(n)<0.215 && seed(n)>=0.189
-            type(n,1)=3;
-            abi(n,1)=1;
-            edu(n,1)=3;
-        elseif seed(n)<0.579 && seed(n)>=0.215
-            type(n,1)=4;
-            abi(n,1)=2;
-            edu(n,1)=1;
-        elseif seed(n)<0.73 && seed(n)>=0.579
-            type(n,1)=5;
-            abi(n,1)=2;
-            edu(n,1)=2;
-        elseif seed(n)<=1 && seed(n)>=0.73
-            type(n,1)=6;
-            abi(n,1)=2;
-            edu(n,1)=3;
-        end
-end
-
-%% Simulation
-tic;
-[c_s,r_s,n_s,u_s,m_s,ch_s,a_s,wh_s,inv_s,wr_s,wn_s] = simulation(params0,G,S,abi,edu,type,C,M,R,N,U);
-toc;
-
-%save simulation.mat
+% %% Simulation
+% tic;
+% [c_s,r_s,n_s,u_s,m_s,ch_s,a_s,wh_s,inv_s,wr_s,wn_s] = simulation(params0,G,S,abi,edu,type,C,M,R,N,U);
+% toc;
+% 
+% save simulation.mat
 
 %% Load Previous Functions & Simulated Data
 load('simulation_July10.mat')
 
 %% Plots
 plots(G,c_s,r_s,n_s,u_s,m_s,ch_s,a_s,wh_s,inv_s,wr_s,wn_s,abi,edu,type)
+
+%% Estimation (WE NEED TO DISCUSS THE ESTIMATION ALGORITHM)
+
+options = optimoptions('fmincon','Algorithm','interior-point','Display','iter');
+[theta,fval,exit,output,lambda]=fminsearch(@(theta) mdf(theta,G,S),params0,[],[],[],[],bounds(:,1),bounds(:,2),[],options);
+theta
+
+exit;
+
+
+
+
---
+++ Year2/Model 1/mdf.m
@@ -0,0 +1,25 @@
+
+function JTN=mdf(params,G,S,abi,edu,type,types,data)  %JTN Objective Function to be minimized
+
+
+S = sspace(params0,G);
+
+parfor z=1:1:G.n_incond
+    z
+    [C(:,:,:,:,:,z),M(:,:,:,:,:,z),R(:,:,:,:,:,z),N(:,:,:,:,:,z),U(:,:,:,:,:,z)]= solution(G,types(z,1),types(z,2),S,params);     toc
+
+end
+
+[c_s,r_s,n_s,u_s,m_s,ch_s,a_s,wh_s,inv_s,wr_s,wn_s] = simulation(params,G,S,abi,edu,type,C,M,R,N,U);
+
+Sbeta=moments(c_s,r_s,n_s,u_s,m_s,ch_s,a_s,wh_s,inv_s,wr_s,wn_s)
+
+% Data Moments
+
+Dbeta=data(:,1);
+Dse=srqt(data(:,2));
+
+JTN=nansum(((Dbeta-Sbeta)./Dse).^2);
+
+
+end
